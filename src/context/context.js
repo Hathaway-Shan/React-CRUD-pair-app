e25e7c1bc043fc98de4836da979c6ac1d4247e65
Hathaway-Shan <hathaway.shan@gmail.com>
2022-10-05 14:10:53 -0700

    added context file and usercontext

---
+++ src/context/context.js
@@ -0,0 +1,13 @@
+import { createContext, useState } from 'react';
+import { getUser } from '../services/auth';
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const currentUser = getUser();
+  const [user, setUser] = useState(currentUser);
+
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+};
+
+export { UserProvider, UserContext };
