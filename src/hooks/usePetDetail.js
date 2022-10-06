c281137a4dc7f01fff174cd71a4926d0cc228a13
Hathaway-Shan <hathaway.shan@gmail.com>
2022-10-06 15:08:50 -0700

    added petDetail component and usePetDetail hook to run get byID route display of single pet tile is working

--- src/hooks/usePetDetail.js
+++ src/hooks/usePetDetail.js
@@ -3,21 +3,20 @@ import { UserContext } from '../context/context';
 import { getPetById } from '../services/pets';
 
 export default function usePetDetail({ id }) {
-  const { pet, setPet } = useContext(UserContext);
+  const { petDetail, setPetDetail } = useContext(UserContext);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        //
         const data = await getPetById(id);
-        setPet(data);
+        await setPetDetail(data);
       } catch (e) {
         // eslint-disable-next-line no-console
         console.log(e.message);
       }
     };
     fetchData();
-  }, [id, setPet]);
+  }, [id, setPetDetail]);
 
-  return { pet };
+  return { petDetail, setPetDetail };
 }
--- src/services/pets.js
+++ src/services/pets.js
@@ -2,11 +2,12 @@ import { checkError, client } from './client';
 
 export async function getPets() {
   const response = await client.from('pets').select('*');
+
   return checkError(response);
 }
 
 export async function getPetById(id) {
-  const response = await client.from('pets').select('*').match({ id: id }).single();
-  console.log('getPetById response is:', response);
+  const response = await client.from('pets').select('*').match({ id }).single();
+
   return checkError(response);
 }
