370dfe59ada9befaf871f63112e5bfcccb3c3e07
Hathaway-Shan <hathaway.shan@gmail.com>
2022-10-05 17:57:23 -0700

    built a logout button instead all auth functionality is working and redirects are working as intended

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Auth from './components/Auth';
 import Pets from './components/PetList';
-
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route path="/auth/:type" component={Auth} />
         <Route path="/pets" component={Pets} />
+        <Route path="*" component={Auth} />
       </Switch>
     </div>
   );
--- src/components/PetList.js
+++ src/components/PetList.js
@@ -1,10 +1,16 @@
+import { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
+import { UserContext } from '../context/context';
 import usePets from '../hooks/usePets';
 import PetCard from './PetCard';
 
-
-
 export default function Pets() {
   const { pets } = usePets();
+  const { user } = useContext(UserContext);
+
+  if (!user) {
+    return <Redirect to="/auth/sign-in" />;
+  }
   return (
     <div>
       {pets.map((pet) => (
