f2b33472ce154b5ac24f912b9caac7b4ea1d5e82
Hathaway-Shan <hathaway.shan@gmail.com>
2022-10-12 12:09:03 -0700

    test to sign in a user is passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,49 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from './App';
+import { UserProvider } from './context/context';
 
-test('renders learn react link', () => {
+//import everything and alias it from a specific file
+import * as authFunctions from './services/auth';
+
+jest.mock('./services/auth');
+
+const mockUser = {
+  id: '0dab2c65-5911-469c-9f12-8fb47ebe52f2',
+  aud: 'authenticated',
+  role: 'authenticated',
+  email: 'dog@example.com',
+};
+
+test('user can sign in', async () => {
+  authFunctions.getUser.mockReturnValue(null);
+  authFunctions.authUser.mockReturnValue(mockUser);
+
+  render(
+    <UserProvider>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </UserProvider>
+  );
+  const headerElem = screen.getByText(/Welcome to Monster Pets!/i);
+  expect(headerElem).toBeInTheDocument();
+
+  const emailInput = screen.getByLabelText(/email/i);
+  fireEvent.change(emailInput, { target: { value: 'dog@example.com' } });
+  expect(emailInput.value).toBe('dog@example.com');
+
+  const passwordInput = screen.getByLabelText(/password/i);
+  fireEvent.change(passwordInput, { target: { value: '123456' } });
+
+  const button = screen.getByRole('button');
+  fireEvent.click(button);
+
+  const headerText = await screen.findByText('hello dog@example.com');
+  expect(headerText).toBeInTheDocument();
+});
+
+test.skip('renders learn react link', () => {
   // render(<App />);
   // const linkElement = screen.getByText(/learn react/i);
   // expect(linkElement).toBeInTheDocument();
